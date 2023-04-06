1af9678d6090aa55cb97f20f5b2c358c6abec5d1
Administrator <admin@example.com>
2023-04-06 15:48:40 +0900

    default session

--- src/main/java/com/example/demosessionapp/DemoSessionAppApplication.java
+++ src/main/java/com/example/demosessionapp/DemoSessionAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
-@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30)
+//@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 30)
 public class DemoSessionAppApplication {
 
     public static void main(String[] args) {
